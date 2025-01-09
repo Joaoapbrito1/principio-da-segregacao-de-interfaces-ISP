d7ba418222a5f34ffa89133689c5c20814969f21
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-09 12:03:55 -0300

    feat: implements the Employee abstract class.

---
+++ src/model/Funcionario.java
@@ -0,0 +1,19 @@
+package model;
+
+public abstract class Funcionario {
+    private String nome;
+    private String cargo;
+
+    public Funcionario(String nome, String cargo) {
+        this.nome = nome;
+        this.cargo = cargo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+}
