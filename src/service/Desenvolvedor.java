22dad8a0d87df8dc4a22cea3108642fbf063220b
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-09 12:10:14 -0300

    feat: change the type of Developer class.

--- src/service/Desenvolvedor.java
+++ src/service/Desenvolvedor.java
@@ -1,11 +1,16 @@
 package service;
 
+import model.Funcionario;
 import model.Programador;
 
-public class Desenvolvedor implements Programador {
+public class Desenvolvedor extends Funcionario implements Programador {
+    public Desenvolvedor(String nome) {
+        super(nome,"Desenvolvedor");
+    }
+
     @Override
     public void programar() {
-        System.out.println("Programando.");
+        System.out.println(getNome() + "está programando.");
 
     }
 }
