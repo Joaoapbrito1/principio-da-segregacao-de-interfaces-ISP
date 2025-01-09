a5f5419f1458b9e1b08605e4617f16a968a4fd99
Joaoapbrito1 <joao.brito@zup.com.br>
2025-01-09 10:41:44 -0300

    refactor: insert output message into the BankAttendant class.

--- src/service/AtendenteBanco.java
+++ src/service/AtendenteBanco.java
@@ -5,6 +5,7 @@ import model.Atendente;
 public class AtendenteBanco implements Atendente {
     @Override
     public void atenderCliente() {
+        System.out.println("Atendendo cliente.");
 
     }
 }
--- src/service/Desenvolvedor.java
+++ src/service/Desenvolvedor.java
@@ -5,6 +5,7 @@ import model.Programador;
 public class Desenvolvedor implements Programador {
     @Override
     public void programar() {
+        System.out.println("Programando.");
 
     }
 }
